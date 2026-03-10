1a747393d639b080f189a6135979c0ba39a7bcf4
Adib Ahmed <adibahmed0700@gmail.com>
2026-03-11 00:49:47 +0530

    Logging.py and Exception.py

--- setup.py
+++ setup.py
@@ -1,34 +1,70 @@
+# Import find_packages and setup functions from setuptools
+# setuptools is used to package and distribute Python projects
 from setuptools import find_packages, setup
+
+# Import List type for type hinting
+# This helps indicate that the function returns a list of strings
 from typing import List
 
-def get_requirements()->List[str]:
+
+# Function to read requirements from requirements.txt
+def get_requirements() -> List[str]:
     """
-    This function will return list of requirements
+    This function reads the requirements.txt file
+    and returns a list of required libraries.
     """
 
-    requirement_lst:List[str]=[]
+    # Create an empty list to store all the dependencies
+    requirement_lst: List[str] = []
+
     try:
-        with open('requirements.txt','r') as file:
-            ## read lines from the file
+        # Open the requirements.txt file in read mode
+        with open('requirements.txt', 'r') as file:
+
+            # Read all lines from the file
+            # Each line represents a dependency
             lines = file.readlines()
-            ## Process each line
+
+            # Process each line in the requirements file
             for line in lines:
+
+                # Remove extra spaces and newline characters
                 requirement = line.strip()
-                ## ignore empty lines ans -e
-                if requirement and requirement!= '-e .':
+
+                # Ignore empty lines and "-e ." entry
+                # "-e ." is used for editable installation in development
+                if requirement and requirement != '-e .':
                     requirement_lst.append(requirement)
+
+    # If requirements.txt does not exist, print an error message
     except FileNotFoundError:
         print("requirements.txt file not found")
 
-    return requirement_lst                  
+    # Return the final list of dependencies
+    return requirement_lst
 
-print(get_requirements())                        
 
+# Print the list of requirements (mainly for testing/debugging)
+print(get_requirements())
+
+
+# setup() function defines metadata and configuration for your project
 setup(
+    # Name of the Python package/project
     name="NetworkSecurity",
+
+    # Version of the package
     version="0.0.1",
+
+    # Author name
     author="Adib Ahmed",
+
+    # Author email address
     author_email="adibahmed0425@gmail.com",
+
+    # Automatically find all packages (folders containing __init__.py)
     packages=find_packages(),
+
+    # Install required dependencies automatically from requirements.txt
     install_requires=get_requirements()
 )
